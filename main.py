f6f5e047453bcb51b35b09dbd48084c1b6d7792b
TF8088 <tiagofarinha03@outlook.com>
2025-01-01 17:46:26 +0000

    Refactor main.py to use SQLite for city data retrieval and update weather data fetching logic

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-#
-#   Trust Wind
-#   Made by: Tiago Farinha, Gonçalo Marinho, Bernardo Melo
-#   Date: 20/11/2024
-#
+# Trust Wind
+# Made by: Tiago Farinha, Gonçalo Marinho, Bernardo Melo
+# Date: 20/11/2024
 
-import logging, os, requests, json, random
+import logging, os, requests, sqlite3
 
 from flask import Flask, redirect, request, render_template, send_file
 from dotenv import load_dotenv
@@ -18,16 +16,35 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 logging.basicConfig(level=logging.DEBUG)
 
-def get_random_cities(num_cities):
-    with open('cyties.json', 'r') as f:
-        cities = json.load(f)
-    return random.sample(cities, num_cities)
+def executar_script(script_file, database_file):
+    conn = sqlite3.connect(database_file)
+    cursor = conn.cursor()
+    with open(script_file, 'r') as script:
+        cursor.executescript(script.read())
+    conn.commit()
+    conn.close()
 
-def get_data(city):
+# executar_script('./private/db/schema.sql', './private/db/db.db')
+
+def get_db_connection():
+    connection = sqlite3.connect('./private/db/db.db')
+    connection.row_factory = sqlite3.Row
+    return connection
+
+
+def get_random_cities(num_cities):
+    connection = get_db_connection()
+    with connection as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM cities ORDER BY RANDOM() LIMIT ?", (num_cities,))
+        return cursor.fetchall()
+
+
+def fetch_weather_data(city):
     url = f"https://api.weatherapi.com/v1/current.json?key={os.getenv('API_KEY')}&q={city}&lang=pt"
-    
+
     response = requests.get(url)
-    
+
     if response.status_code == 200:
         data = response.json()
 
@@ -36,10 +53,11 @@ def get_data(city):
             'temperatura': data['current']['temp_c'],
             'icon': data['current']['condition']['icon']
         }
-        
-        return res        
+
+        return res
     else:
         return {'erro': 'Não foi possível obter os dados'}
+
 
 @app.route("/city")
 def city():
@@ -47,26 +65,17 @@ def city():
 
     print(city)
 
+    # TODO 
+
     return render_template('/pages/city.html', title=os.getenv('TITLE'), city=city)
+
 
 @app.route("/")
 def index():
     random_cities = get_random_cities(3)
+    cities_data = []
+    for city in random_cities:
+        city_data = fetch_weather_data(city['name'])
+        cities_data.append(city_data)
 
-    city1 = get_data(random_cities[0])  # This line should be indented at the same level as the following lines
-    city2 = get_data(random_cities[1])
-    city3 = get_data(random_cities[2])
-
-    return render_template('/pages/index.html', title=os.getenv('TITLE'),
-                           city_box1=city1['cidade'], 
-                           temp_box1=city1['temperatura'], 
-                           icon_box1=city1['icon'],
-
-                           city_box2=city2['cidade'], 
-                           temp_box2=city2['temperatura'], 
-                           icon_box2=city2['icon'],
-                           
-                           city_box3=city3['cidade'], 
-                           temp_box3=city3['temperatura'], 
-                           icon_box3=city3['icon'],
-                           )
+    return render_template('/pages/index_.html', title=os.getenv('TITLE'), cities=cities_data)
